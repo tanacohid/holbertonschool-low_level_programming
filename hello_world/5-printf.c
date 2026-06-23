191ad6b66dccbf758288038f6f99acce39f96c15
tanacohid <jeandbf5@gmail.com>
2026-06-23 17:59:27 +0200

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

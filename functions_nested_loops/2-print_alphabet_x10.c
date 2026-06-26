3f8ff43c3749cc233bb8b5473bf0789591079468
tanacohid <jeandbf5@gmail.com>
2026-06-26 19:38:14 +0200

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_alphabet - ta race betty
+*/
+
+void print_alphabet_x10(void)
+{
+	int c;
+	int i;
+
+	c = 'a';
+	i = '0';
+
+	for (i = 0; i < 10; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		(c = 'a');
+	}
+}

9ac072157c2220838d308c8d69713b735a02ad21
tanacohid <jeandbf5@gmail.com>
2026-06-28 17:13:21 +0200

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an integer
+* @n: integer to compute
+*
+* Return: the absolute value of n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+
+	return (n);
+}

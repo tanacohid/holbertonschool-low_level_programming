98595411fc1ddd65684ad9630ff22b178bf98b06
tanacohid <jeandbf5@gmail.com>
2026-06-30 19:09:26 +0200

    task 10

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints to 0 to 98
+* @n: number to check
+*
+* Return: the value of the last digit
+*/
+void print_to_98(int n)
+{
+n = 0;
+
+for (n = 0 ; 98 ; n++)
+	_putchar(n);
+	_putchar(',');
+	_putchar(' ');
+}

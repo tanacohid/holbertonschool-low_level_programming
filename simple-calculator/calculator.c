57752d9e59602546faa71a53fdd9953df7769732
tanacohid <jeandbf5@gmail.com>
2026-06-30 16:52:21 +0200

    task 0

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -3,19 +3,18 @@ int main (void)
 {
     int choice = 1;
     /**
-    * int - a; b
-    *
-     */
+    * int - a, b
+    
     int a = 0;
     int b = 0;
     int result = 0;
-
+    */
     printf ("simple Calculator\n");
 
     while (choice != 0)
     {
-        printf ("1) add\n2) Substact\n3) Multiply\n4) Divide\n0) Quit\n")
-        printf ("choice ?\n")
+        printf ("1) add\n2) Substact\n3) Multiply\n4) Divide\n0) Quit\n");
+        printf ("choice ?\n");
         scanf ("%d", &choice);
         if (choice == 0)
         {

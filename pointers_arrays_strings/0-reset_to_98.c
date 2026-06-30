fcf344c19a86dccddb6598f605cd6b01f9c88425
tanacohid <jeandbf5@gmail.com>
2026-06-30 17:20:26 +0200

    task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,11 @@
 #include "main.h"
 #include "main.c"
 #include <stdio.h>
-
+/**
+* reset_to_98 - to beat or not to beat
+* @n: not to beat
+*/
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }

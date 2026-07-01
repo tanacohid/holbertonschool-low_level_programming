a8542d5dceec58a67b060e1c48284e88dfe03046
tanacohid <jeandbf5@gmail.com>
2026-07-01 15:50:13 +0200

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,39 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
- * @s: pointer to the string
- */
+* _strlen - compte le nombre de caractere donner
+* @s: est la suite de char
+* Return: a
+*/
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+
+	while (*s != '\0')
+	{
+		a++;
+		s++;
+	}
+	return (a);
+}
+
+/**
+* print_rev - prints a string in reverse
+* @s: pointer to the string
+*/
 void print_rev(char *s)
 {
-    int i;
+	int i;
 
-    i = _strlen(s) - 1;
+	i = _strlen(s) - 1;
 
-    while (i >= 0)
-    {
-        _putchar(s[i]);
-        i--;
-    }
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }

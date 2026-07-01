b74ecd7904019eb7fc2da1fb3bd5d0b33712e60b
tanacohid <jeandbf5@gmail.com>
2026-07-01 23:40:07 +0200

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,32 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: pointer to the string
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+* puts2 - prints every other character of a string
+* @str: pointer to the string
+*/
 void puts2(char *str)
 {
-    int i;
-    i = 0;
+	int i;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i += 2;
-    }
-    _putchar('\n');
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
 }

b4b2edffd79a029402ce5e37d67a3a17e4a2b881
tanacohid <jeandbf5@gmail.com>
2026-07-01 22:51:28 +0200

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - compte le nombre de caractere donner
+ * @s: est la suite de char
+ * Return: a
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+
+	while (*s != '\0')
+	{
+		a++;
+		s++;
+	}
+	return (a);
+}
 
 /**
  * rev_string - reverses a string

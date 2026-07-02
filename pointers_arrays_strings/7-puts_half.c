7716e00cf2205423158070550183a4d5bf5d8c9d
tanacohid <jeandbf5@gmail.com>
2026-07-02 11:20:51 +0200

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string
+* @str: pointer to the string
+*/
+void puts_half(char *str)
+{
+    int i;
+    int t;    
+
+    i = (_strlen(str) + 1) / 2;
+    t = _strlen(str);
+    
+    for (i = (_strlen(str) + 1) / 2; str[i] != str[t]; i++)
+        _putchar(str[i]);
+    _putchar('\n');
+}

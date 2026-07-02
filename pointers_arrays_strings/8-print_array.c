bdc2bd7973b48d869e2fadbee637b9c02d9b815c
tanacohid <jeandbf5@gmail.com>
2026-07-02 14:36:31 +0200

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* puts_half - prints every other character of a string
+* @str: pointer to the string
+*/
+void print_array(int *a, int n)
+{
+    int i;
+    int t;
+
+    i = _strlen(a);
+    t = 0;
+
+    for (t = 0; *a[t] = *a[i]; t++)
+
+        printf("%d, "; *a[t])
+}

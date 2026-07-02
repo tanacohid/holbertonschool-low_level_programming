7ff4fe019deb0a66c6f42aa3a84bae13acf46613
tanacohid <jeandbf5@gmail.com>
2026-07-02 19:39:00 +0200

    task 0

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * _strlen - compte le nombre de caractere donner
- * @s: est la suite de char
- * Return: a
- */
-int _strlen(char *s)
-{
-	int a;
-
-	a = 0;
-
-	while (*s != '\0')
-	{
-		a++;
-		s++;
-	}
-	return (a);
-}
 
 /**
 * puts_half - prints every other character of a string

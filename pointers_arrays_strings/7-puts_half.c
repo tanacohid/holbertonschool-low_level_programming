e8e7a8bed457d1a208180311e612e960d04e9b80
tanacohid <jeandbf5@gmail.com>
2026-07-03 23:21:40 +0200

    task 8

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,6 @@
 #include <unistd.h>
 #include "main.h"
 #include <stdio.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _strlen - compte le nombre de caractere donner
- * @s: est la suite de char
- * Return: a
- */
-int _strlen(char *s)
-{
-	int a;
-
-	a = 0;
-
-	while (*s != '\0')
-	{
-		a++;
-		s++;
-	}
-	return (a);
-}
 
 /**
 * puts_half - prints every other character of a string

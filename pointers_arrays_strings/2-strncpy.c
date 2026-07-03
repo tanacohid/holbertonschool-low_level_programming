5932b548c9cc6eada498951126a3fbebd7dec569
tanacohid <jeandbf5@gmail.com>
2026-07-03 02:28:02 +0200

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,13 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _strncpy - copies a string
- * @dest: destination buffer
- * @src: source string
- * @n: maximum number of characters to copy
- *
- * Return: pointer to dest
- */
+* _strncpy - copies a string
+* @dest: destination buffer
+* @src: source string
+* @n: maximum number of characters to copy
+*
+* Return: pointer to dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;

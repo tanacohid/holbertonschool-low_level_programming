6a4529e9b8777ec0c1fe6cbeb8b96dad38c1b6b1
tanacohid <jeandbf5@gmail.com>
2026-07-03 17:28:29 +0200

    task 5

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,25 +1,70 @@
 #include <stdio.h>
 int main (void)
 {
-    int choice = 1;
-    /**
-    * int - a, b
-    
-    int a = 0;
-    int b = 0;
-    int result = 0;
-    */
-    printf ("simple Calculator\n");
+	int choice = 1;
+	
+	int a = 0;
+	int b = 0;
+	int result = 0;
+	
+	printf ("simple Calculator\n");
 
-    while (choice != 0)
-    {
-        printf ("1) add\n2) Substact\n3) Multiply\n4) Divide\n0) Quit\n");
-        printf ("choice ?\n");
-        scanf ("%d", &choice);
-        if (choice == 0)
-        {
-            printf ("bye!");
-        }
-    }
-    return (0);
+	while (choice != 0)
+	{
+		if (choice > -1 && choice < 5)
+		{
+			printf ("1) add\n2) Substact\n3) Multiply\n4) Divide\n0) Quit\n");
+		}
+		printf ("choice ?\n");
+		scanf ("%d", &choice);
+		if (choice < 0 || choice > 4)
+		{
+			printf ("Invalid choice");
+		}
+		if (choice == 0)
+		{
+			printf ("bye!\n");
+		}
+		else if (choice == 1)
+		{
+			printf ("A: ");
+			scanf("%d", &a);
+			printf ("B: ");
+			scanf("%d", &b);
+			result = a + b;
+			printf("Result = %d\n", result);
+		}
+		else if (choice == 2)
+		{
+			printf ("A: ");
+			scanf("%d", &a);
+			printf ("B: ");
+			scanf("%d", &b);
+			result = a - b;
+			printf("Result = %d\n", result);
+		}
+		else if (choice == 3)
+		{
+			printf ("A: ");
+			scanf("%d", &a);
+			printf ("B: ");
+			scanf("%d", &b);
+			result = a * b;
+			printf("Result = %d\n", result);
+		}
+		else if (choice == 4)
+		{
+			printf ("A: ");
+			scanf("%d", &a);
+			printf ("B: ");
+			scanf("%d", &b);
+			if (b ==0)
+			{
+				printf("")
+			}
+			result = a + b;
+			printf("Result = %d\n", result);
+		}
+	}
+	return (0);
 }

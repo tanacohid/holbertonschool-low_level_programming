3d1623d3233130dc4d75e0d00e1ad0b286c6f299
tanacohid <jeandbf5@gmail.com>
2026-07-07 11:21:09 +0200

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,35 +3,35 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
- char *_strdup(char *str)
+* main - check the code
+*
+* Return: Always 0.
+*/
+char *_strdup(char *str)
 {
-    int i = 0;
-    int p = 0;
-    char *t;
+	int i = 0;
+	int p = 0;
+	char *t;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-
-    for (i = 0; str[i] != '\0'; i++)
-    {}
-
-    t = malloc(sizeof(char) * (i + 1));
-
-    if (t == NULL)
+	if (str == NULL)
 	{
-        return (NULL);
-    }
+		return (NULL);
+	}
 
-    for (p = 0; p <= i ; p++)
-    {
-        t[p] = str[p];
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{}
 
-    return (t);
+	t = malloc(sizeof(char) * (i + 1));
+
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+
+	for (p = 0; p <= i ; p++)
+	{
+		t[p] = str[p];
+	}
+
+	return (t);
 }

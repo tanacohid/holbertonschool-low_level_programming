e8eea44755cac89ef6bcb4a05bba8f818c17141f
tanacohid <jeandbf5@gmail.com>
2026-07-09 21:24:04 +0200

    feat: implement init_dog function to initialize dog structure fields

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,3 +7,11 @@
  * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}

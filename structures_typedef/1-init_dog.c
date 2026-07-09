626fbeba3081512266e835cd978d0ff112f5b6f6
tanacohid <jeandbf5@gmail.com>
2026-07-09 21:28:34 +0200

    fix: correct NULL pointer check in init_dog function

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,18 +1,17 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to the structure
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }

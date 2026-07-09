2ea4488b1b90aa16f09015cf6c469d8b0aae2952
tanacohid <jeandbf5@gmail.com>
2026-07-09 17:57:00 +0200

    implement _calloc function for memory allocation and add simple_print_buffer for buffer printing

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to the allocated memory, or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	p = ptr;
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (ptr);
+}

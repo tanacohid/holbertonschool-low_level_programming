ceb011471a8f80d047f1af068d3912d1b6ebf0fd
tanacohid <jeandbf5@gmail.com>
2026-07-15 16:38:29 +0200

    feat: implement print_name function and main program for function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name =! NULL && f != NULL)
+    {
+        f(name);
+    }
+}

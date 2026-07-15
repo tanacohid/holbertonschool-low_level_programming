4af95655b623e4e7866e8bc0ad20fbf80200546f
tanacohid <jeandbf5@gmail.com>
2026-07-15 19:31:54 +0200

    feat: implement int_index function to search for an integer using a comparison function

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer using a comparison function
+ * @array: pointer to the array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer to the comparison function
+ *
+ * Return: the index of the first element for which cmp does not return 0,
+ * -1 if no element matches or if an error occurs
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array <= NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}

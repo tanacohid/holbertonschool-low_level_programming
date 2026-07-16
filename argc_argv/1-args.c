1e6cafcf4f625a716dbd46a88a0d7cc76ea2964b
tanacohid <jeandbf5@gmail.com>
2026-07-16 18:58:25 +0200

    feat: add implementation of 1-args program to display argument count

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - affiche le nom de la commande
+ * @argv: tableau du nombre de programe
+ * @argc: nombre total d argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc);
+	return (0);
+}

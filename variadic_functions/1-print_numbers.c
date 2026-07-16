ef288c97f633caec53ce7b5a63a1c09f10ea55a5
tanacohid <jeandbf5@gmail.com>
2026-07-16 15:05:15 +0200

    fix: ensure separator is set to an empty string instead of NULL in print_numbers function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			separator = "";
 
 		if (i == n - 1)
-			separator = NULL;
+			separator = "";
 
 		printf("%d%s", va_arg(args, int), separator);
 	}

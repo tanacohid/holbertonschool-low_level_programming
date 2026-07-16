f4739a95693718ca4169663a30b1b29cbcf6acec
tanacohid <jeandbf5@gmail.com>
2026-07-16 15:14:10 +0200

    fix: add missing semicolon in print_numbers function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,11 +11,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned char i;
 
 	if (n == 0)
-		printf("\n")
+	{
+		printf("\n");
 		return;
-
+	}
 	va_start(args, n);
-
 
 	for (i = 0; i < n; i++)
 	{

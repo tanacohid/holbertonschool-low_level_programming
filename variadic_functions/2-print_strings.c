a2d57ea86c294115711ec203424d1603dfbcee74
tanacohid <jeandbf5@gmail.com>
2026-07-16 16:39:17 +0200

    feat: add print_strings function to handle variable string arguments

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print tout les argument va_list
+ * @separator: separt deux argument de va_list
+ * @n: le nombre d'argument de va_list
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned char i;
+	char *t;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			separator = "";
+
+		if (i == n - 1)
+			separator = "";
+
+		t = va_arg(args, *char)
+		if (t == NULL)
+			*t = "";
+
+		printf("%s%s", va_arg(args, *char), separator);
+	}
+	va_end(args);
+	printf("\n");
+}

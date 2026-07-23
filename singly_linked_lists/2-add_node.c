0e639276eef2dcd05235b1ee7769615fa89827e8
tanacohid <jeandbf5@gmail.com>
2026-07-23 17:57:56 +0200

    fix: correct string termination condition in add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -25,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	for (i = 0; str[i] != \n; i++)
+	for (i = 0; str[i] != '\n'; i++)
 	{}
 	new->len = str[i];
 	new->next = *head;

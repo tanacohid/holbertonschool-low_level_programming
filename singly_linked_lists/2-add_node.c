aeae7f21978463ac8339062ed674b67050cf5ed5
tanacohid <jeandbf5@gmail.com>
2026-07-23 17:53:16 +0200

    fix: correct length calculation in add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int i = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -24,7 +25,9 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	for (i = 0; str[i] != \n; i++)
+	{}
+	new->len = str[i];
 	new->next = *head;
 	*head = new;
 

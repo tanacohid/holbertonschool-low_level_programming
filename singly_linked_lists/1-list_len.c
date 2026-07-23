14d3b6a027a8153bd204ee91cedcfbe14bf6dae3
tanacohid <jeandbf5@gmail.com>
2026-07-23 16:03:55 +0200

    fix: correct function documentation for list_len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - prints all the elements of a list_t list
  * @h: pointer to the first node
  *
  * Return: the number of nodes

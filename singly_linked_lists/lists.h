b4d26346b1eaeb9f9bffec5c57e29c4a207e4f99
tanacohid <jeandbf5@gmail.com>
2026-07-23 15:20:33 +0200

    fix: reorder print_list function declaration in lists.h for consistency

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-size_t print_list(const list_t *h);
 typedef struct list_s
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
 
 #endif

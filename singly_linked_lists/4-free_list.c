fd0dbe721b89ed9027355bb3ba35fcac61424aef
tanacohid <jeandbf5@gmail.com>
2026-07-24 17:06:14 +0200

    feat: add free_list function to deallocate memory for list_t nodes

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp->str);
+		free(temp);
+	}
+}

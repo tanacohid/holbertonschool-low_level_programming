09617f2413e44c7648a65780037d42d41e136e50
tanacohid <jeandbf5@gmail.com>
2026-07-28 10:39:12 +0200

    feat: implement doubly linked list with print function and main test

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	for (;h != NULL; count++)
+	{
+		printf("%d", h->n);
+		h = h->next;
+	}
+	return (count)
+}

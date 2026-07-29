a71ddcde46de0de8483db0c88ae32a9233268571
tanacohid <jeandbf5@gmail.com>
2026-07-29 10:42:46 +0200

    style: format code for better readability in insert_dnodeint_at_index function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,27 +9,30 @@
  *
  * Return: address of the new node, or NULL if it failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
+		unsigned int idx, int n)
 {
 	dlistint_t *new, *tmp;
-	size_t i = 0;
+	unsigned int i = 0;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	
-	tmp =*h;
 
-	for (; tmp != NULL && i < idx - 1; i++)
+	tmp = *h;
+
+	while (tmp != NULL && i < idx - 1)
+	{
 		tmp = tmp->next;
+		i++;
+	}
 
 	if (tmp == NULL)
 		return (NULL);
-	
-	if (tmp-next == NULL)
-		return (add_dnodeint_end(h, n));
-	
-	new = malloc(sizeof(dlistint_t));
 
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 

a8a1115b4754c048f1f04eab489defe85109c330
tanacohid <jeandbf5@gmail.com>
2026-07-29 10:33:32 +0200

    feat: add insert_dnodeint_at_index function to insert a new node at a specified index in a doubly linked list and create main function for testing

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: address of the head pointer
+ * @idx: index where the new node should be added
+ * @n: integer to store in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp;
+	size_t i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	
+	tmp =*h;
+
+	for (; tmp != NULL && i < idx - 1; i++)
+		tmp = tmp->next;
+
+	if (tmp == NULL)
+		return (NULL);
+	
+	if (tmp-next == NULL)
+		return (add_dnodeint_end(h, n));
+	
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	new->prev = tmp;
+	new->next = tmp->next;
+
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
+}
